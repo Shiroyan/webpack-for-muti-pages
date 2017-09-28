da636da379def9286a57726c6d58f86f3c8055a6
yanjiaxin <yjx@szltech.com>
2017-09-28 15:20:51 +0800

    支持同时创建多个文件

--- build/create.js
+++ build/create.js
@@ -1,12 +1,16 @@
 var fs = require('fs')
 var chalk = require('chalk')
-var ora = require('ora')
 var root = require('./pages.json').root
 
-var spinner = ora('Creating ... \n')
-spinner.start()
-var dirname = process.argv[2]
-var path = `${root}/${dirname}`
+var dirnames = process.argv.slice(2)
+var paths = (function () {
+  var paths = [];
+  for (var dirname of dirnames) {
+    paths.push(`${root}/${dirname}`)
+  }
+  return paths
+})()
+
 var htmlTemplate = `<!DOCTYPE html>
 <html lang="en">
 
@@ -22,36 +26,43 @@ var htmlTemplate = `<!DOCTYPE html>
 
 </html>`
 
-if (!dirname) {
+if (!dirnames.length) {
   console.error(chalk.bgRed('Please input the dirname !!!\n'))
   return
 }
-if (fs.existsSync(path)) {
-  console.error(chalk.bgRed('File is already exists !!!\n'))
-  return
+
+function isExists(path) {
+  return fs.existsSync(path);
 }
 
-function throwErr (err) {
+function throwErr(err) {
   if (err) {
     throw err
   }
 }
 
-function updatePages() {
+function updatePages(dirname) {
   var path = 'build/pages.json'
-  var rc = JSON.parse(fs.readFileSync(path, {encoding: 'utf-8'}))
+  var rc = JSON.parse(fs.readFileSync(path, { encoding: 'utf-8' }))
   rc.pages = rc.pages.concat(dirname)
   var wc = JSON.stringify(rc)
   fs.writeFileSync(path, wc)
 }
 
-fs.mkdir(path , (err) => {
-  if (!err) {
-    fs.writeFile(`${path}/index.html`, htmlTemplate, throwErr);
-    fs.writeFile(`${path}/index.js`, '', throwErr);
-    fs.writeFile(`${path}/index.css`, '', throwErr);
-    updatePages();
-    spinner.stop()
-    console.log(chalk.bgGreen(' Create success.\n'));
+(function () {
+  for (var dirname of dirnames) {
+    var path = `${root}/${dirname}`
+    if (isExists(path)) {
+      console.error(chalk.bgRed(`${path} is already exists !!!\n`))
+      return false
+    } else {
+      fs.mkdirSync(path)
+      fs.writeFile(`${path}/index.html`, htmlTemplate, throwErr)
+      fs.writeFile(`${path}/index.js`, '', throwErr)
+      fs.writeFile(`${path}/index.css`, '', throwErr)
+      updatePages(dirname)
+    }
   }
-})
+  console.log(chalk.bgGreen(' Create success.\n'))
+})();
+
